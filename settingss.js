46d8c493f52e06319d3d8ecff7e6fb3a660b19b2
medowmax <mudzirodly@gmail.com>
2025-08-22 15:28:20 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~lzF3zJQI#L_U71R1KcjrlOi5hsVHf5fdiRlRbYlivMmFq28m5bNY",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263712781953", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
